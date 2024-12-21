6e7357ad90461f91177608e6143aa644f03c62af
Jason Martin <jhmartin@toger.us>
2024-12-21 03:21:07 +0000

    Trying out more API GW

--- apigateway.tf
+++ apigateway.tf
@@ -22,11 +22,12 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.pyspy.id
-  resource_id             = aws_api_gateway_resource.pyspy.id
-  http_method             = aws_api_gateway_method.proxy.http_method
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
   integration_http_method = "GET"
-  type                    = "MOCK"
+  type = "AWS"
+  uri = aws_lambda_function.html_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "proxy" {
@@ -55,4 +56,50 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
   stage_name = "dev" # replace with aws_api_gateway_stage
+}
+
+data "archive_file" "lambda_package" {
+  type = "zip"
+  source_file = "index.js"
+  output_path = "index.zip"
+}
+
+resource "aws_lambda_function" "html_lambda" {
+  filename = "index.zip"
+  function_name = "myLambdaFunction"
+  role = aws_iam_role.lambda_role.arn
+  handler = "index.handler"
+  runtime = "nodejs14.x"
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+    {
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }
+  ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.html_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }
