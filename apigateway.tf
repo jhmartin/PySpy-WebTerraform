33520baba497cfc79c9c6e74fdf001dfdc77b8ff
Jason Martin <jhmartin@toger.us>
2024-12-21 03:09:29 +0000

    Building out per https://spacelift.io/blog/terraform-api-gateway

--- apigateway.tf
+++ apigateway.tf
@@ -13,3 +13,37 @@ resource "aws_api_gateway_resource" "pyspy" {
   parent_id   = aws_api_gateway_rest_api.pyspy.root_resource_id
   path_part   = "character_intel"
 }
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.pyspy.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.pyspy.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.pyspy.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.pyspy.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
