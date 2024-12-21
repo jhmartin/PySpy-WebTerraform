4003b063894c9b175e1f8dc133263e669f58ba9d
Jason Martin <jhmartin@toger.us>
2024-12-21 03:10:47 +0000

    Fix references

--- apigateway.tf
+++ apigateway.tf
@@ -16,14 +16,14 @@ resource "aws_api_gateway_resource" "pyspy" {
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.pyspy.id
-  resource_id   = aws_api_gateway_resource.root.id
+  resource_id   = aws_api_gateway_resource.pyspy.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.pyspy.id
-  resource_id             = aws_api_gateway_resource.root.id
+  resource_id             = aws_api_gateway_resource.pyspy.id
   http_method             = aws_api_gateway_method.proxy.http_method
   integration_http_method = "POST"
   type                    = "MOCK"
@@ -31,14 +31,14 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_method_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_resource.pyspy.id
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = "200"
 }
 
 resource "aws_api_gateway_integration_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_resource.pyspy.id
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = aws_api_gateway_method_response.proxy.status_code
 
