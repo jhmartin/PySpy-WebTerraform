f63c8138acb9281fdca5d31f7a7bd56203346f7a
Jason Martin <jhmartin@toger.us>
2024-12-21 01:15:31 +0000

    Add a DDB equivalent of the SQL intel summary table

---
+++ dynamodb.tf
@@ -0,0 +1,14 @@
+# The web backend always fetches by user id. AWS has a free tier for DDB so we'll use that.
+
+resource "aws_dynamodb_table" "pyspy_intel" {
+  name           = "pyspy-intel" # Set the name of your DynamoDB table
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "character_id" # Specify your table's hash key
+
+  attribute {
+    name = "character_id"
+    type = "N" # 'S' for string, 'N' for number, 'B' for binary
+  }
+}
