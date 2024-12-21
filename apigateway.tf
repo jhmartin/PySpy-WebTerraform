947d4f169d3a68dc248ae0dc47fc65c128845d16
Jason Martin <jhmartin@toger.us>
2024-12-21 03:26:47 +0000

    Convert GET to POST for lambda

--- apigateway.tf
+++ apigateway.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
   resource_id = aws_api_gateway_resource.pyspy.id
   http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type = "AWS"
   uri = aws_lambda_function.html_lambda.invoke_arn
 }
