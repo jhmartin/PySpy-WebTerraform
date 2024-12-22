1d59d1d01723d5e32871d33ac7677e8e66dd5ddb
Jason Martin <jhmartin@toger.us>
2024-12-22 18:33:33 +0000

    Typo

--- cloudfront.tf
+++ cloudfront.tf
@@ -60,7 +60,7 @@ resource "aws_cloudfront_cache_policy" "pyspy" {
   parameters_in_cache_key_and_forwarded_to_origin {
 
     cookies_config {
-      cookie_behavior = "nond"
+      cookie_behavior = "none"
     }
     headers_config {
       header_behavior = "none"
