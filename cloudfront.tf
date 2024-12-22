9917db1f77ad4b7c43c504bea1339e1b761dde46
Jason Martin <jhmartin@toger.us>
2024-12-22 16:59:43 +0000

    Actually add cloudfront file

---
+++ cloudfront.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = regex("//(.*?)/", aws_api_gateway_stage.devstage.invoke_url)
+    origin_id   = "apigateway"
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "PySpy"
+
+  #  aliases = ["pyspy.toger.us"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "apigateway"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 5
+    max_ttl                = 5
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
