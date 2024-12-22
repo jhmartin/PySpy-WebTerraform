184b2ae428312c8eda5913c5f2caff0a0eebb191
Jason Martin <jhmartin@toger.us>
2024-12-22 15:54:20 +0000

    Remove dupe stage

--- apigateway.tf
+++ apigateway.tf
@@ -55,7 +55,6 @@ resource "aws_api_gateway_deployment" "deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
-  stage_name  = "stg" # replace with aws_api_gateway_stage
 }
 
 resource "aws_api_gateway_stage" "devstage" {
