aff285507f3e1fd1abef4a5f6532b906712ed3bd
Jason Martin <jhmartin@toger.us>
2024-12-22 18:15:42 +0000

    Try a custom origin config

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,7 +1,19 @@
 resource "aws_cloudfront_distribution" "distribution" {
   origin {
+  }
+
+  origin {
     domain_name = element(regex("^https?://([^/:]+)", aws_api_gateway_stage.devstage.invoke_url),0)
     origin_id   = "apigateway"
+
+    custom_origin_config {
+      http_port                = 80   # The HTTP port the custom origin listens on
+      https_port               = 443  # The HTTPS port the custom origin listens on
+      origin_protocol_policy   = "match-viewer"  # Match the protocol with the viewer
+      origin_ssl_protocols     = ["TLSv1.2", "TLSv1.3"]
+      origin_keepalive_timeout = 5
+      origin_read_timeout      = 30
+    }
   }
 
   enabled         = true
