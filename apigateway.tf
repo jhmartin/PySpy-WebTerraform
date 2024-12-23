c28b646c81dfa9cb3fa7bc2806158f66e1cae109
Jason Martin <jhmartin@toger.us>
2024-12-23 04:34:31 +0000

    Deployments aren't working quite right

--- apigateway.tf
+++ apigateway.tf
@@ -55,6 +55,25 @@ resource "aws_api_gateway_deployment" "deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.pyspy.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.pyspy.id,
+      aws_api_gateway_method.proxy.id,
+      aws_api_gateway_integration.lambda_integration.id,
+    ]))
+  }
 }
 
 resource "aws_api_gateway_stage" "devstage" {
@@ -64,9 +83,9 @@ resource "aws_api_gateway_stage" "devstage" {
 }
 
 data "archive_file" "lambda_package" {
-  type             = "zip"
-  source_file      = "pyspy.py"
-  output_path      = "pyspy.zip"
+  type        = "zip"
+  source_file = "pyspy.py"
+  output_path = "pyspy.zip"
 }
 
 resource "aws_lambda_function" "html_lambda" {
