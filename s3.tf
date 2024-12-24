1a58eb41dbe6651ec0182d90ff611651656cdf42
Jason Martin <jhmartin@toger.us>
2024-12-24 02:18:32 +0000

    Add OAC bucket policy

--- s3.tf
+++ s3.tf
@@ -12,3 +12,31 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 
   provider = aws.ue1
 }
+
+resource "aws_s3_bucket_policy" "cloudfront" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.cloudfront_oac_access.json
+}
+
+data "aws_iam_policy_document" "cloudfront_oac_access" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.pyspy_static.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.distribution.arn]
+    }
+  }
+}
