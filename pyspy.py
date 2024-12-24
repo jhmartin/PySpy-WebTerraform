46df0b0218d60c5c705284f232628ba9f9fdda68
Jason Martin <jhmartin@toger.us>
2024-12-24 04:58:57 +0000

    Handle people who never had a killmail

--- pyspy.py
+++ pyspy.py
@@ -1,6 +1,6 @@
-import boto3
 import json
 import decimal
+import boto3
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -9,25 +9,28 @@ class DecimalEncoder(json.JSONEncoder):
             return int(o)
         return super().default(o)
 
-def lambda_handler(event, context):
+
+def lambda_handler(event, _):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('pyspy-intel')
 
     character_id = event['queryStringParameters']['character_id']
-    
+
     # Fetch the item from DynamoDB
     response = table.get_item(
         Key={
             'character_id': int(character_id)
         }
-     )
-    
+    )
+
+    result = {'character_id': int(character_id)}
     item = response.get('Item', {})
-    
+    item = item | result
+
     return {
         'statusCode': 200,
         'body': json.dumps(item, cls=DecimalEncoder),
         'headers': {
             'Content-Type': 'application/json'
         },
-     }
+    }
